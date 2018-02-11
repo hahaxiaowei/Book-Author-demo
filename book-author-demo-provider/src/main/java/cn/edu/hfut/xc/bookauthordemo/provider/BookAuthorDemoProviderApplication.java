36c35b6767274adf3d5196e04d4755cdc140b48b
sunwei <1103361580@qq.com>
2018-02-11 16:47:43 +0800

    代码提交

--- book-author-demo-provider/src/main/java/cn/edu/hfut/xc/bookauthordemo/provider/BookAuthorDemoProviderApplication.java
+++ book-author-demo-provider/src/main/java/cn/edu/hfut/xc/bookauthordemo/provider/BookAuthorDemoProviderApplication.java
@@ -1,11 +1,8 @@
 package cn.edu.hfut.xc.bookauthordemo.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
